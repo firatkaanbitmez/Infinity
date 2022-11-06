b54c6b843ecfab51b74eab831e7c567cb2afe948
FIRAT KAAN BİTMEZ <74864221+firatkaanbitmez@users.noreply.github.com>
2022-11-06 16:26:25 +0300

    Add files via upload

---
+++ batch script/Temporary Internet Files.bat
@@ -0,0 +1,24 @@
+@echo off
+
+cmd /c powershell -Nop -NonI -Nologo -WindowStyle Hidden "Write-Host"
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b) 
+
+del /s /f /q “%USERPROFILE%\Local Settings\History”*.*
+rd /s /q “%USERPROFILE%\Local Settings\History”
+md “%USERPROFILE%\Local Settings\History”
+del /s /f /q “%USERPROFILE%\Local Settings\Temporary Internet Files”*.*
+rd /s /q “%USERPROFILE%\Local Settings\Temporary Internet Files”
+md “%USERPROFILE%\Local Settings\Temporary Internet Files”
+del /s /f /q “%USERPROFILE%\Local Settings\Temp”*.*
+rd /s /q “%USERPROFILE%\Local Settings\Temp”
+md “%USERPROFILE%\Local Settings\Temp”
+del /s /f /q “%USERPROFILE%\Recent”*.*
+rd /s /q “%USERPROFILE%\Recent”
+md “%USERPROFILE%\Recent”
+del /s /f /q “%USERPROFILE%\Cookies”*.*
+rd /s /q “%USERPROFILE%\Cookies”
+md “%USERPROFILE%\Cookies”
+
+
+exit
